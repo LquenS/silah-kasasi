aca704a1601accd5accbb2269c973e5f985f47b5
LquenS <35933753+LquenS@users.noreply.github.com>
2020-05-28 19:14:06 +0300

    Add files via upload

---
+++ Silah-Kasa.sql
@@ -0,0 +1,7 @@
+USE `essentialmode`;
+
+
+INSERT INTO `items` (`name`, `label`, `limit`) VALUES
+  ('silah-kasasi', 'Silah Kasası', 1)
+
+;
---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Silah Kasası'
+
+version '0.0.1'
+
+
+server_scripts {
+    'server/server.lua'
+}
+dependencies {
+    'es_extended'
+}
